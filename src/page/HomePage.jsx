1b9d59d480546a76eccf1065d8e87d963662d592
Nicholas Vandyck <nichovansankara@gmail.com>
2024-03-23 23:56:58 +0000

    image sliding

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,54 +1,82 @@
 import React, { useState, useEffect } from 'react';
-import '../style/slide.css'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
 import Header from '../components/Header';
 
-
 const HomePage = () => {
-   const images = [
-    "../assets/images/image-1.jpg",
-    "../assets/images/image-2.jpg",
-    "../assets/images/image-3.jpg"
-   ]
-
-   const [currentIndex, setCurrentIndex] = useState(0)
-
-   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
-    }, 5000); // Change image every 5 seconds
-
-    return () => clearInterval(interval);
-  }, [images.length]);
-
-  const handleArrowClick = direction => {
-    if (direction === 'prev') {
-      setCurrentIndex(prevIndex => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
-    } else {
-      setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
+  const slides = [
+    {
+      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
     }
-  };
-  
-    return (
-        <div>
-            <Header />
-            <div
-      className="background-slider"
-      style={{
-        backgroundImage: `url('${images[currentIndex]}')`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        position: 'full',
-        height: '100vh',
-        transition: 'background-image 0.3s ease',
-      }}
-    >
-      <button onClick={() => handleArrowClick('prev')}>Previous</button>
-      <button onClick={() => handleArrowClick('next')}>Next</button>
    
-    </div>
-      
-      </div>
-  )
-}
+  ];
 
-export default HomePage
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const isLastSlide = currentIndex === slides.length - 1;
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex(newIndex);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex, slides.length]);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+        <div
+          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+          className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+        ></div>
+        {/* Left Arrow */}
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        {/* Right Arrow */}
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+        <div className='flex top-4 justify-center py-2'>
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className='text-2xl cursor-pointer'
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
